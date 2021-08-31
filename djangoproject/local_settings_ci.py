f09e57bb0fe94e36ba08d0b4e9279d8460283d1c
Nicolas Noé <nicolas.noe@inbo.be>
2021-08-31 14:16:29 +0200

    Django CI: fix

---
+++ djangoproject/local_settings_ci.py
@@ -0,0 +1,21 @@
+from .settings import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '<something secret here>'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'riparias-early-warning-webapp',
+    }
+}
+
+# A Gbif.org is necessary to automatically download occurrences via the `import_occurrences` command
+RIPARIAS_GBIF_USERNAME = 'xxx'
+RIPARIAS_GBIF_PASSWORD = 'yyy'
