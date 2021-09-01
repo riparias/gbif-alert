3ed415c6fb46e630267d88f4f9f46cfb833dc3e4
Nicolas Noé <nicolas.noe@inbo.be>
2021-09-01 12:29:18 +0200

    JS integration: we can now load single file components from .vue files.

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
+import Hello from './Hello.vue';
 
 var app = new Vue({
   el: '#app',
+  components: {
+    Hello
+  },
   delimiters: ['[[', ']]'],
   data: {
     message: 'Hello Vue!'
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,33 @@
 const path = require('path');
-const { VueLoaderPlugin } = require('vue-loader');
+const {VueLoaderPlugin} = require('vue-loader');
 
 module.exports = {
-  entry: './assets/index.js',  // path to our input file
-  output: {
-    filename: 'index-bundle.js',  // output bundle file name
-    path: path.resolve(__dirname, './static'),  // path to our Django static directory
-  },
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader'
-      },
-    ]
-  },
-  plugins: [
-    new VueLoaderPlugin()
-  ],
-  resolve: {
-      alias: {
-         vue: 'vue/dist/vue.js'
-      }
+    entry: './assets/index.js',  // path to our input file
+    output: {
+        filename: 'index-bundle.js',  // output bundle file name
+        path: path.resolve(__dirname, './static'),  // path to our Django static directory
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /.css$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader',
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ],
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.js'
+        }
     }
 };
