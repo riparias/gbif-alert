256884c49690a601c29cb16c0be713e8f3929915
Nicolas Noé <nicolas.noe@inbo.be>
2021-09-28 13:04:34 +0200

    Add link to repo + git commit hash. Fix #31, #27

--- dashboard/context_processors.py
+++ dashboard/context_processors.py
@@ -1,4 +1,5 @@
 from dashboard.models import DataImport
+from dashboard.utils import human_readable_git_version_number
 
 
 def latest_data_import_processor(request):
@@ -6,4 +7,7 @@ def latest_data_import_processor(request):
         data_import = DataImport.objects.latest("end")
     except DataImport.DoesNotExist:
         data_import = None
-    return {"latest_data_import": data_import}
+    return {
+        "latest_data_import": data_import,
+        "git_version_number": human_readable_git_version_number,
+    }
--- dashboard/utils.py
+++ dashboard/utils.py
@@ -1,3 +1,12 @@
+import subprocess
+
+
 def readable_string(input_string: str) -> str:
     """Remove multiple whitespaces and \n to make a long string more readable"""
     return " ".join(input_string.replace("\n", "").split())
+
+
+def human_readable_git_version_number() -> str:
+    return subprocess.check_output(
+        ["git", "describe", "--always"], encoding="UTF-8"
+    ).strip()
