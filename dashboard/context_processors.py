cf78b98cd97134ca2055803ea4329739b0f2abb3
Nicolas Noé <nicolas.noe@inbo.be>
2021-11-03 10:52:52 +0100

    Attempt to fix #54

--- dashboard/context_processors.py
+++ dashboard/context_processors.py
@@ -4,7 +4,7 @@ from dashboard.utils import human_readable_git_version_number
 
 def latest_data_import_processor(request):
     try:
-        data_import = DataImport.objects.latest("end")
+        data_import = DataImport.objects.latest("id")
     except DataImport.DoesNotExist:
         data_import = None
     return {
