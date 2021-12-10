c22b037b768d5d9590661b31f9215036d596a277
Nicolas Noé <nicolas.noe@inbo.be>
2021-12-10 11:35:22 +0100

    Small addition to the occurrences import test

--- dashboard/tests/commands/test_import_occurrences.py
+++ dashboard/tests/commands/test_import_occurrences.py
@@ -16,6 +16,17 @@ class ImportOccurrencesTest(TransactionTestCase):
         Species.objects.create(name="Lixus bardanae", gbif_taxon_key=1224034)
         Species.objects.create(name="Polydrusus planifrons", gbif_taxon_key=7972617)
 
+    def test_gbif_request_not_necessary(self) -> None:
+        """No HTTP request emitted if the --source-dwca option is used"""
+        with open(SAMPLE_DATA_PATH / "gbif_download.zip", "rb") as gbif_download_file:
+            with requests_mock.Mocker() as m:
+                call_command(
+                    "import_occurrences",
+                    source_dwca=gbif_download_file,
+                )
+                request_history = m.request_history
+                self.assertEqual(len(request_history), 0)
+
     def test_gbif_request(self) -> None:
         """The correct HTTP requests are emitted to gbif.org"""
         with open(SAMPLE_DATA_PATH / "gbif_download.zip", "rb") as gbif_download_file:
