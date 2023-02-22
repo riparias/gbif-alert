72a9f4ce59e1406c01871ab6bf782b17d31a9721
Nicolas Noé <nicolas@niconoe.eu>
2023-02-22 14:22:04 +0100

    The button to delete an alert now asks confirmation via a modal. Fix #160.

---
+++ assets/ts/components/DeleteAlertButton.vue
@@ -0,0 +1,39 @@
+<template>
+  <button-with-confirmation
+      class="btn btn-danger btn-sm float-end riparias-delete-alert-button"
+      :button-id="buttonId"
+      confirmation-message-body="Do you really want to this alert? This can't be undone"
+      @user-confirmed="userConfirmedDeletion()">
+      <i class="bi bi-trash"></i> Delete this alert
+  </button-with-confirmation>
+
+  <form method="post" ref="form" class="d-inline float-end riparias-alert-delete-form" :action="formAction">
+    <slot></slot>
+  </form>
+
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+import ButtonWithConfirmation from "./ButtonWithConfirmation.vue";
+
+export default defineComponent({
+  name: "DeleteAlertButton",
+  props: {
+    formAction: {
+      type: String,
+      required: true,
+    },
+    buttonId: {
+      type: String,
+      required: true,
+    },
+  },
+  components: {ButtonWithConfirmation},
+  methods: {
+    userConfirmedDeletion: function () {
+      (this.$refs.form as any).submit();
+    },
+  },
+});
+</script>
