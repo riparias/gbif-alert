e05c8c7891e53e26a6bd0668723a3f56af9fa4ba
Nicolas Noé <nicolas@niconoe.eu>
2023-03-23 15:28:10 +0100

    Species selector: it's now possible to filter per tag

--- assets/ts/components/SpeciesSelector.vue
+++ assets/ts/components/SpeciesSelector.vue
@@ -1,8 +1,19 @@
 <template>
   <div class="pterois-species-selector">
     <div class="small mb-3 input-group">
-      <input class="form-control form-control-sm" type="text" placeholder="type here to filter species..." aria-label="search form" v-model="search">
-      <button class="btn btn-outline-secondary btn-sm" type="button" id="button-addon2" v-if="search != ''" @click="search=''"><i class="bi bi-x-circle-fill"></i></button>
+      <input class="form-control form-control-sm" type="text" placeholder="type here to filter species..." aria-label="search form" v-model="textFilter">
+      <button class="btn btn-outline-secondary btn-sm" type="button" id="button-addon2" v-if="textFilter != ''" @click="textFilter=''"><i class="bi bi-x-circle-fill"></i></button>
+    </div>
+    <div class="small">
+      Filter by tags:
+      <span v-for="tag in availableTags"
+            :style="getStyleForTag(tag)"
+            :class="tagFilter === tag ? 'tag-filter-enabled' : 'tag-filter-disabled'"
+            class="badge bg-secondary mx-1"
+            @click="tagFilter = tagFilter === tag ? '' : tag"
+      >
+        {{ tag }}
+        </span>
     </div>
 
     <table v-if="lines.length > 0" class="table table-hover table-sm">
@@ -47,7 +58,7 @@
           <td>{{ line.gbifTaxonKey }}</td>
           <td>
             <span v-for="tag in line.tags"
-                  :style="'background-color: '  + wordToColor(tag) + '!important' + '; ' + 'color: ' + legibleColor(wordToColor(tag)) + '!important'"
+                  :style="getStyleForTag(tag)"
                   class="badge bg-secondary mx-1">
               {{ tag }}
             </span>
@@ -75,21 +86,29 @@ const props = defineProps<{
 // Reactive data
 const selectedSpeciesIds = ref<number[]>(props.modelValue);
 const sortBy = ref<string>('scientific');
-const search = ref<string>('');
+const textFilter = ref<string>('');
+const tagFilter = ref<string>('');
 
 // Computed data
 const lines = computed(() => {
   // All visible lines, sorted and filtered
   let species = props.availableSpecies.slice();
 
+  // Filtering by text
   species = species.filter((s) => {
-    const scientificNameMatches = s.scientificName.toLowerCase().includes(search.value.toLowerCase());
-    const vernacularNameMatches = s.vernacularName.toLowerCase().includes(search.value.toLowerCase());
-    const gbifKeyMatches = s.gbifTaxonKey.toString().includes(search.value);
+    const scientificNameMatches = s.scientificName.toLowerCase().includes(textFilter.value.toLowerCase());
+    const vernacularNameMatches = s.vernacularName.toLowerCase().includes(textFilter.value.toLowerCase());
+    const gbifKeyMatches = s.gbifTaxonKey.toString().includes(textFilter.value);
 
     return scientificNameMatches || vernacularNameMatches || gbifKeyMatches;
   });
 
+  // Filtering by tag
+  if (tagFilter.value !== '') {
+    species = species.filter((s) => s.tags.includes(tagFilter.value));
+  }
+
+  // Sorting
   species = species.sort((a, b) => {
     if (sortBy.value === 'scientific') {
       return a.scientificName.localeCompare(b.scientificName);
@@ -118,6 +137,22 @@ const allVisibleLinesSelected = computed({
   }
 });
 
+const availableTags = computed(() => {
+  // All available tags
+  const tags = new Set<string>();
+  props.availableSpecies.forEach((s) => {
+    s.tags.forEach((t) => tags.add(t));
+  });
+  return Array.from(tags).sort();
+});
+
+// Methods
+const getStyleForTag = (tag: string) => {
+  return {
+    backgroundColor: wordToColor(tag) + '!important',
+    color: legibleColor(wordToColor(tag)) + '!important'
+  }
+}
 
 // Various
 const emit = defineEmits(['update:modelValue']);
@@ -125,3 +160,12 @@ watch(selectedSpeciesIds, (newVal) => {
   emit('update:modelValue', newVal)
 })
 </script>
+
+<style scoped>
+.tag-filter-enabled {
+  opacity: 1;
+}
+.tag-filter-disabled {
+  opacity: 0.4;
+}
+</style>
