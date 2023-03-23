976efe91259d4835322a6b7b72e86ab25c581a1f
Nicolas Noé <nicolas@niconoe.eu>
2023-03-23 12:15:12 +0100

    Reimplementation of the new species selector using a table

--- assets/ts/components/FilterSelectorModal.vue
+++ assets/ts/components/FilterSelectorModal.vue
@@ -6,7 +6,7 @@
       role="dialog"
       @click="modalClicked($event);"
   >
-    <div class="modal-dialog" role="document">
+    <div class="modal-dialog modal-lg" role="document">
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title">{{ props.modalTitle }}</h5>
--- assets/ts/components/SpeciesSelector.vue
+++ assets/ts/components/SpeciesSelector.vue
@@ -1,40 +1,112 @@
 <template>
   <div class="pterois-species-selector">
-    <p v-for="species in props.availableSpecies" class="form-check">
-      <input
-          class="form-check-input"
-          type="checkbox"
+    <div class="small mb-3 input-group">
+      <input class="form-control form-control-sm" type="text" placeholder="type here to filter species..." aria-label="search form" v-model="search">
+      <button class="btn btn-outline-secondary btn-sm" type="button" id="button-addon2" v-if="search != ''" @click="search=''"><i class="bi bi-x-circle-fill"></i></button>
+    </div>
 
-          :value="species.id"
-          :id="'pterois-species-selector-entry-' + species.id"
-          v-model="selectedSpeciesIds"
-      />
-      <label
-          class="form-check-label"
-          :for="'pterois-species-selector-entry-' + species.id"
-      >
-        <span class="fst-italic"> {{ species.scientificName }}</span> ({{ species.vernacularName }})
-      </label>
-    </p>
+    <table v-if="lines.length > 0" class="table table-hover table-sm">
+      <thead>
+        <tr>
+          <th scope="col"><input
+                class="form-check-input"
+                type="checkbox"
+                v-model="allVisibleLinesSelected"
+            />
+          </th>
+
+          <th scope="col" :class="{'text-primary': sortBy === 'scientific'}" @click="sortBy = 'scientific'">
+            Scientific name
+          </th>
+
+          <th scope="col" :class="{'text-primary': sortBy === 'vernacular' }" @click="sortBy = 'vernacular'">
+            Vernacular name
+          </th>
+
+          <th scope="col">
+            GBIF taxon key
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="line in lines" :key="line.id">
+          <td>
+            <input
+                class="form-check-input"
+                type="checkbox"
+                :value="line.id"
+                v-model="selectedSpeciesIds"
+            />
+          </td>
+          <td><i>{{ line.scientificName }}</i></td>
+          <td>{{ line.vernacularName }}</td>
+          <td>{{ line.gbifTaxonKey }}</td>
+        </tr>
+      </tbody>
+    </table>
+
+    <p v-else class="small">No matching species found.</p>
 
   </div>
 </template>
 
 <script setup lang="ts">
 import {SpeciesInformation} from "@/assets/ts/interfaces";
-import {ref, watch} from "vue";
+import {computed, ref, watch} from "vue";
 
+// Props
 const props = defineProps<{
   availableSpecies: SpeciesInformation[]
-  modelValue: number[]
+  modelValue: number[] // Array of selected species IDs
 }>()
 
+// Reactive data
 const selectedSpeciesIds = ref<number[]>(props.modelValue);
+const sortBy = ref<string>('scientific');
+const search = ref<string>('');
 
+// Computed data
+const lines = computed(() => {
+  // All visible lines, sorted and filtered
+  let species = props.availableSpecies.slice();
+
+  species = species.filter((s) => {
+    const scientificNameMatches = s.scientificName.toLowerCase().includes(search.value.toLowerCase());
+    const vernacularNameMatches = s.vernacularName.toLowerCase().includes(search.value.toLowerCase());
+
+    return scientificNameMatches || vernacularNameMatches;
+  });
+
+  species = species.sort((a, b) => {
+    if (sortBy.value === 'scientific') {
+      return a.scientificName.localeCompare(b.scientificName);
+    } else {
+      return a.vernacularName.localeCompare(b.vernacularName);
+    }
+  });
+
+  return species;
+});
+
+const allVisibleLinesSelected = computed({
+  get: () => {
+    // True if all visible lines are selected
+    return lines.value.every((line) => selectedSpeciesIds.value.includes(line.id));
+  },
+  set: (value) => {
+    // Select or deselect all visible lines
+    if (value) {
+      selectedSpeciesIds.value = lines.value.map((line) => line.id);
+    } else {
+      selectedSpeciesIds.value = [];
+    }
+  }
+});
+
+
+// Various
 const emit = defineEmits(['update:modelValue']);
-
 watch(selectedSpeciesIds, (newVal) => {
   emit('update:modelValue', newVal)
 })
 </script>
-
