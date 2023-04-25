cdede2d4ea0cacecdd8c6530492d68e64d57fe31
Nicolas Noé <nicolas@niconoe.eu>
2023-04-25 14:13:58 +0200

    Fix mypy issues

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -5,7 +5,6 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.core.exceptions import ValidationError
-from django.utils.functional import _StrPromise
 from django.utils.translation import gettext_lazy as _
 
 from dashboard.models import ObservationComment, Alert, Area, User
@@ -58,7 +57,7 @@ class AlertForm(forms.ModelForm):
         return cleaned_data
 
 
-def _enabled_languages_as_tuple() -> Tuple[Tuple[str, _StrPromise], ...]:
+def _enabled_languages_as_tuple():
     """
     Return a tuple of tuples of the form (language_code, language_name) for all enabled languages
 
