ab4a8d4d32c451746019aee838c291e96f15476e
Nicolas Noé <nicolas@niconoe.eu>
2023-09-18 10:34:38 +0200

    Added import script for natura 2000 areas in Belgium

---
+++ dashboard/management/commands/load_n2000_areas.py
@@ -0,0 +1,42 @@
+"""Custom script to import the N2000 areas for the LIFE RIPARIAS project.
+
+The usual load_area.py script cannot be used because the specifities of the data make it unsuitable for the usual
+LayerMapping helper.
+"""
+import os
+
+from django.contrib.gis.gdal import DataSource
+from django.core.management import BaseCommand
+
+from dashboard.models import Area, DATA_SRID
+
+THIS_FILE_DIR = os.path.dirname(__file__)
+SOURCE_DATA_GPKG_PATH = (
+    f"{THIS_FILE_DIR}/../../../source_data/public_areas/natura2000/natura2000_BE.gpkg"
+)
+
+
+class Command(BaseCommand):
+    help = "Import N2000 areas in the database. "
+
+    def handle(self, *args, **options) -> None:
+        self.stdout.write("Importing N2000 areas")
+        self.stdout.write("Opening source file")
+        ds = DataSource(SOURCE_DATA_GPKG_PATH)
+        layer = ds[0]
+        self.stdout.write(f"Found {layer.num_feat} features")
+        for feature in layer:
+            area_name = f"{feature['SITECODE']} - {feature['SITENAME']}"
+            tags = ["Natura2000"]
+            tags.append(f"sitetype {feature['SITETYPE']}")
+            if feature["Brussels"].value == 1:
+                tags.append("Brussels")
+            if feature["Wallonia"].value == 1:
+                tags.append("Wallonia")
+            if feature["Flanders"].value == 1:
+                tags.append("Flanders")
+
+            self.stdout.write(f"Creating area {area_name}, with tags {tags}")
+            reprojected_geom = feature.geom.transform(DATA_SRID, clone=True)
+            area = Area.objects.create(mpoly=reprojected_geom.wkt, name=area_name)  # type: ignore
+            area.tags.add(*tags)
