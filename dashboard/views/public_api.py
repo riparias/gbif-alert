bdefb60242b759a3f68299e394eb4efd944d00e0
Nicolas Noé <nicolas@niconoe.eu>
2024-03-25 12:29:11 +0100

    WFS server improvements: (more) WIP

--- dashboard/views/public_api.py
+++ dashboard/views/public_api.py
@@ -3,6 +3,7 @@ from django.contrib.gis.geos import GEOSGeometry
 from django.db.models import Count
 from django.http import JsonResponse, HttpRequest
 from gisserver.features import FeatureType, field  # type: ignore
+from gisserver.types import XsdElement, XsdTypes
 from gisserver.views import WFSView  # type: ignore
 
 from dashboard.models import Species, DATA_SRID, Observation
@@ -53,44 +54,81 @@ def species_per_polygon_json(request: HttpRequest) -> JsonResponse:
     return JsonResponse(r, safe=False)
 
 
+# Unfortunately I can't make the  "flattened feature" approach of django-gisserver to
+# work, so I have to define custom classes for all the "complex" fields I want to expose
+# I also have to override the type in the constructor if it ius not an integer otherwise
+# the metadata will be incorrect and values will appear as NULL in QGIS, for example.
+# Beware: the interactions with model_attributes is quite messy and not well documented
+# in the django-gisserver documentation. This is ugly, but this is the only way I found.
+class XSDElementForceStringType(XsdElement):
+    """Custom helper to force a string type in the XSD metadata"""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.type = XsdTypes.string
+
+
+class CustomXsdElementGBIFTaxonKey(XsdElement):
+    def get_value(self, instance):
+        return instance.species.gbif_taxon_key
+
+
+class CustomXsdElementScientificName(XSDElementForceStringType):
+    def get_value(self, instance):
+        return instance.species.name
+
+
 class ObservationsWFSView(WFSView):
     feature_types = [
         FeatureType(
-            Observation.objects.all(),
+            Observation.objects.filter(coordinate_uncertainty_in_meters__lt=5),
+            # fields=[
+            #     "gbif_id",
+            #     "stable_id",
+            #     # field("species-scientific-name", model_attribute="species.name"),
+            #     # field("species_id", model_attribute="species.id"),
+            #     field(
+            #         # "species.vernacular_name_nl",
+            #         "species-vernacular-name-nl",
+            #         model_attribute="observation.species.vernacular_name_nl",
+            #     ),
+            #     # field(
+            #     #     "species-vernacular-name-en",
+            #     #     model_attribute="species.vernacular_name_en",
+            #     # ),
+            #     # field(
+            #     #     "species-vernacular-name-fr",
+            #     #     model_attribute="species.vernacular_name_fr",
+            #     # ),
+            #     # field("dataset-name", model_attribute="source_dataset.name"),
+            #     # field(
+            #     #     "dataset-gbif-key",
+            #     #     model_attribute="source_dataset.gbif_dataset_key",
+            #     # ),
+            #     "location",
+            #     field("observation_date", model_attribute="date"),
+            #     "individual_count",
+            #     "locality",
+            #     "municipality",
+            #     "basis_of_record",
+            #     "recorded_by",
+            #     "coordinate_uncertainty_in_meters",
+            #     "references",
+            # ],
             fields=[
-                "gbif_id",
-                # field("data_provider_occurrence_id", model_attribute="occurrence_id"),
-                "stable_id",
-                # field("species-id", model_attribute="species_id"),
-                # ".species.name",
-                field("species-name", model_attribute="species.name"),
-                # field(
-                #     "species",
-                #     fields=[
-                #         "id",
-                #         "name",
-                #         "vernacular_name_nl",
-                #         "vernacular_name_en",
-                #         "vernacular_name_fr",
-                #     ],
-                # ),
-                field(
-                    "dataset",
-                    fields=[
-                        "name",
-                        "gbif_dataset_key",
-                    ],
-                    model_attribute="source_dataset",
-                ),
                 "location",
-                field("observation_date", model_attribute="date"),
-                "individual_count",
-                "locality",
-                "municipality",
-                "basis_of_record",
-                "recorded_by",
-                "coordinate_uncertainty_in_meters",
-                "references",
+                "gbif_id",
+                "stable_id",
+                field(
+                    "species_gbif_key",
+                    model_attribute="species",
+                    xsd_class=CustomXsdElementGBIFTaxonKey,
+                ),
+                field(
+                    "species_scientific_name",
+                    model_attribute="species",
+                    xsd_class=CustomXsdElementScientificName,
+                ),
             ],
         ),
     ]
