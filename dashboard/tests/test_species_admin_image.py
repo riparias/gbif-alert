437879b2bc6dc25ef2fd2ec3ccd6fb71f04d1045
Nicolas Noé <nicolas@niconoe.eu>
2026-06-26 08:35:58 +0200

    test(admin): assert persisted provenance + tidy imports

--- dashboard/tests/test_species_admin_image.py
+++ dashboard/tests/test_species_admin_image.py
@@ -1,7 +1,10 @@
 import pytest
 from django.contrib.admin.sites import AdminSite
+from django.contrib.auth import get_user_model
 from dashboard.admin import SpeciesAdmin
 from dashboard.models import Species
+
+User = get_user_model()
 
 
 class _Form:
@@ -15,6 +18,7 @@ def test_admin_manual_url_sets_provenance_manual():
                  image_url="https://example.org/x.jpg")
     admin = SpeciesAdmin(Species, AdminSite())
     admin.save_model(request=None, obj=sp, form=_Form(["image_url"]), change=False)
+    sp.refresh_from_db()
     assert sp.image_source_type == "manual"
 
 
@@ -24,13 +28,12 @@ def test_admin_clearing_url_resets_provenance():
                  image_url="", image_source_type="wikipedia")
     admin = SpeciesAdmin(Species, AdminSite())
     admin.save_model(request=None, obj=sp, form=_Form(["image_url"]), change=True)
+    sp.refresh_from_db()
     assert sp.image_source_type == ""
 
 
 @pytest.mark.django_db
 def test_species_admin_change_form_renders(client):
-    from django.contrib.auth import get_user_model
-    User = get_user_model()
     User.objects.create_superuser(username="admin", password="pw", email="a@b.c")
     client.login(username="admin", password="pw")
     sp = Species.objects.create(name="Renderus testus", gbif_taxon_key=999099)
